91db13c14798d5a7b1bb2d866c52e0160e37c329
Arunuday <debnath.arunuday007@gmail.com>
2025-12-24 13:39:30 +0530

    font change

--- src/Components/settings_page/Setting.jsx
+++ src/Components/settings_page/Setting.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Setting = () => {
   return (
     <div>
-      <h1 className="text-yellow-50">hello setting page</h1>
+      <h1 className="text-yellow-50 font-extrabold">hello setting page</h1>
     </div>
   );
 };
